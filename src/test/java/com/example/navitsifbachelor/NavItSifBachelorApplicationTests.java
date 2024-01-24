01c4a22f03a87f66c9b49c558e3700281e6f7f5e
jpwiig <jpwiig@gmail.com>
2024-01-24 15:08:22 +0100

    Initial commit

---
+++ src/test/java/com/example/navitsifbachelor/NavItSifBachelorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.navitsifbachelor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavItSifBachelorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
