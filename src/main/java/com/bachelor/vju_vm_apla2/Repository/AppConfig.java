6a3c1b0885a7d5e9a61c42df344296570b798d28
Ali Imran Anjum <89576402+Aliimrananjum@users.noreply.github.com>
2024-02-06 15:32:10 +0100

    kommentarer

--- src/main/java/com/bachelor/vju_vm_apla2/Repository/AppConfig.java
+++ src/main/java/com/bachelor/vju_vm_apla2/Repository/AppConfig.java
@@ -11,6 +11,7 @@ import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options
 @Configuration
 public class AppConfig {
 
+    //setter opp wiremock server og enabler logging
     @Bean(initMethod = "start", destroyMethod = "stop")
     public WireMockServer wireMockServer() {
         return new WireMockServer(options().port(8081)
