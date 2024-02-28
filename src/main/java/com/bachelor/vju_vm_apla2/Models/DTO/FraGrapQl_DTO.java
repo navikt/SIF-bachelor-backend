f52042e46f77484ea549af8cd5961d3e9de85b3f
Ali Imran Anjum <89576402+Aliimrananjum@users.noreply.github.com>
2024-02-28 09:51:51 +0100

    Added POJO from schema.graphql through maven plugin

--- src/main/java/com/bachelor/vju_vm_apla2/Models/DTO/FraGrapQl_DTO.java
+++ src/main/java/com/bachelor/vju_vm_apla2/Models/DTO/FraGrapQl_DTO.java
@@ -1,6 +1,6 @@
 package com.bachelor.vju_vm_apla2.Models.DTO;
 
-import com.bachelor.vju_vm_apla2.graphql.Journalpost;
+import com.bachelor.vju_vm_apla2.Models.POJO.graphql.Journalpost;
 import lombok.Data;
 
 import java.util.List;
---
+++ src/main/java/com/bachelor/vju_vm_apla2/Models/POJO/graphql/Journalposttype.java
@@ -0,0 +1,36 @@
+package com.bachelor.vju_vm_apla2.Models.POJO.graphql;
+
+/**
+ * Sier hvorvidt journalposten er et inngående dokument, et utgående dokument eller et notat.
+ */
+@jakarta.annotation.Generated(
+    value = "com.kobylynskyi.graphql.codegen.GraphQLCodegen",
+    date = "2024-02-28T09:45:41+0100"
+)
+public enum Journalposttype {
+
+    /**
+     * **Inngående dokument** - Dokumentasjon som NAV har mottatt fra en ekstern part. De fleste inngående dokumenter er søknader, ettersendelser av dokumentasjon til sak, eller innsendinger fra arbeidsgivere. Meldinger brukere har sendt til "Skriv til NAV" arkiveres også som inngående dokumenter..
+     */
+    I("I"),
+    /**
+     * **Unngående dokument** - Dokumentasjon som NAV har produsert og sendt ut til en ekstern part. De fleste utgående dokumenter er informasjons- eller vedtaksbrev til privatpersoner eller organisasjoner. "Skriv til NAV"-meldinger som saksbehandlere har sendt til brukere arkiveres også som utgående dokumenter.
+     */
+    U("U"),
+    /**
+     * **Notat** - Dokumentasjon som NAV har produsert selv, uten at formålet er å distribuere dette ut av NAV. Eksempler på notater er samtalereferater med veileder på kontaktsenter og interne forvaltningsnotater.
+     */
+    N("N");
+
+    private final String graphqlName;
+
+    private Journalposttype(String graphqlName) {
+        this.graphqlName = graphqlName;
+    }
+
+    @Override
+    public String toString() {
+        return this.graphqlName;
+    }
+
+}
