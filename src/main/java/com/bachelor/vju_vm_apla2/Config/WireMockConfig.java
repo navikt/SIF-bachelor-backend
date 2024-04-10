8c5280eee8f59a726bb64bee8b047241fc61d56e
jpwiig <jpwiig@gmail.com>
2024-04-10 13:26:37 +0200

    global variable for files in wiremock

--- src/main/java/com/bachelor/vju_vm_apla2/Config/WireMockConfig.java
+++ src/main/java/com/bachelor/vju_vm_apla2/Config/WireMockConfig.java
@@ -22,6 +22,8 @@ public class WireMockConfig {
     public int portnr;
     @Value("${db.url}")
     public String url;
+    @Value("${db.files}")
+    public String files;
     @Bean(initMethod = "start", destroyMethod = "stop")
     public WireMockServer wireMockServer() {
         return new WireMockServer(options()
@@ -30,7 +32,7 @@ public class WireMockConfig {
                 .extensions(new DynamiskPdfStubRespons())
                 /* Below is the classpath which is "." meaning current classpath root, where WireMock will look
                    for files such as stubs. */
-                .usingFilesUnderClasspath("/vju/resources/__files") //change this one for not in use with docker //IMPORTANT: CHANGE THIS WHEN NOT CREATING DOCKER IMAGES TO .
+                .usingFilesUnderClasspath(files) //change this one for not in use with docker //IMPORTANT: CHANGE THIS WHEN NOT CREATING DOCKER IMAGES TO .
                 // Below configures a logger with WireMock, which logs info to the console for debugging
                 .notifier(new ConsoleNotifier(true)));
     }
