aa5ff8e4f8d0ef8f4bbb3b262c12f8f536f44b1e
jpwiig <jpwiig@gmail.com>
2024-04-22 09:03:45 +0200

    added some potenial configurations, they need some fixin, but the bare minimum

--- src/main/java/com/bachelor/vju_vm_apla2/Config/SecurityConfig.java
+++ src/main/java/com/bachelor/vju_vm_apla2/Config/SecurityConfig.java
@@ -7,13 +7,19 @@ import no.nav.security.token.support.client.spring.oauth2.ClientConfigurationPro
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client;
 
 import no.nav.security.token.support.client.spring.oauth2.OAuth2ClientRequestInterceptor;
+import no.nav.security.token.support.core.configuration.MultiIssuerConfiguration;
+import no.nav.security.token.support.filter.JwtTokenValidationFilter;
+import no.nav.security.token.support.jaxrs.servlet.JaxrsJwtTokenValidationFilter;
+import no.nav.security.token.support.spring.MultiIssuerProperties;
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.springframework.boot.web.client.RestClientCustomizer;
 import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.reactive.function.client.WebClient;
 
 
@@ -24,19 +30,27 @@ import java.util.Map;
    JWT validation on incoming requests. So any request that doesn't have a valid JWT token will be rejected */
 //todo: might need to reconfigure this to work with a dockerfile, might need to change url only
 @EnableOAuth2Client(cacheEnabled = true)
+//enable ouath2client also makes sure that the tokens are vaildated through the webclient as well as
 @Configuration
 @EnableJwtTokenValidation
 
 public class SecurityConfig {
-/*
-//according to docs - it is auto configurable in spring boot sooo...
-@Bean
-public RestClientCustomizer customizer(OAuth2ClientRequestInterceptor reqInterceptor) {
-    return client -> client.requestInterceptor(reqInterceptor);
-}
-@Bean
+
+    //according to docs - it is auto configurable in spring boot sooo...
+/*@Bean
+public WebClientCustomizer customizer(OAuth2ClientRequestInterceptor reqInterceptor) {
+    return client -> client.(reqInterceptor);
+}*/
+/*@Bean
     public OAuth2ClientRequestInterceptor oAuth2ClientRequestInterceptor(ClientConfigurationProperties properties, OAuth2AccessTokenService service, ClientConfigurationPropertiesMatcher matcher) {
     return new OAuth2ClientRequestInterceptor(properties, service, matcher);
 }*/
-
+/*    @Bean
+    public MultiIssuerProperties multiIssuerConfiguration() {
+        return new MultiIssuerProperties();
+    }
+    @Bean
+    public FilterRegistrationBean<JwtTokenValidationFilter> oidcTokenValidationFilterBean(MultiIssuerConfiguration config) {
+        return new FilterRegistrationBean<>(new JaxrsJwtTokenValidationFilter(config));
+    }*/
 }
